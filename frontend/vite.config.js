7e2a50f2001923ad1327829efa054337e92558ef
Aashish pandey <pandey786aashish@gmail.com>
2024-11-14 07:08:58 +0530

    error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{port:5173}
-})
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+};
