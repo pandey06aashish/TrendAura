f5307c42320fda833a431d2dae090face9d4cde3
Aashish pandey <pandey786aashish@gmail.com>
2024-11-14 15:42:49 +0530

    correct error config file

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
-})
+  server: {
+    port: 5174,
+  },
+  build: {
+    rollupOptions: {
+      // Explicitly mark "react-router-dom" as external
+      external: ['react-router-dom'],
+    },
+  },
+});
