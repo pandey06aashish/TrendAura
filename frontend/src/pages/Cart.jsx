5fb9bd9354808bc9f3e6c8b7c778e5a91d737a7a
Aashish pandey <pandey786aashish@gmail.com>
2024-11-14 06:56:47 +0530

    correct error

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/cartTotal';
+
+
 const Cart = () => {
   const { products, currency, cartItems, updateCartItem, removeCartItem ,updateQuantity,navigate} = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
