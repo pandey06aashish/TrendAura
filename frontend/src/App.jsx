21039de09745246b41791756356f9c4e51693f55
Aashish pandey <pandey786aashish@gmail.com>
2024-11-14 07:19:32 +0530

    error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
