73834ad3edc6b5a9d401b8d73a6ab03fa22a8af0
Aashish pandey <pandey786aashish@gmail.com>
2024-11-15 09:39:27 +0530

    vircel error

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,15 +6,4 @@ export default defineConfig({
   server: {
     port: 5174,
   },
-  build: {
-    rollupOptions: {
-      external: [], // Leave this empty if you don't need to externalize specific modules
-    },
-  },
-  resolve: {
-    alias: {
-      // Ensure Vite can correctly resolve react-router-dom
-      'react-router-dom': require.resolve('react-router-dom'),
-    },
-  },
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,
-  },
-  build: {
-    rollupOptions: {
-      external: [], // Leave this empty if you don't need to externalize specific modules
-    },
-  },
-  resolve: {
-    alias: {
-      // Ensure Vite can correctly resolve react-router-dom
-      'react-router-dom': require.resolve('react-router-dom'),
-    },
-  },
-});
+  server:{port:5173}
+})
