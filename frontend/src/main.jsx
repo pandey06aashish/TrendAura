7e309ccc27d815033e352ed3cbab638a8342b97d
Aashish pandey <pandey786aashish@gmail.com>
2024-11-15 21:41:32 +0530

    error in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import ShopContextProvider from './context/ShopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>  
+  <BrowserRouter
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>,
-)
+);
