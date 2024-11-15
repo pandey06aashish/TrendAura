f6eeb0d1a16b3690217b3f0f6495ce5cdd65d681
Aashish pandey <pandey786aashish@gmail.com>
2024-11-15 10:17:28 +0530

    admin vite

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 5174 },
+  build: {
+    rollupOptions: {
+      external: [
+        'react-router-dom',
+        'react-toastify',
+        'axios', // Ensure axios is explicitly marked external
+      ],
+    },
+  },
 })
