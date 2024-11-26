8bee37952909bf802b4d27e94624116889810873
Aashish pandey <pandey786aashish@gmail.com>
2024-11-26 22:36:18 +0530

    correct error in admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,32 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    port: 5174,
+  server: {   port: 5174,
   },
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'], // Externalize only necessary libraries
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Group other dependencies into a vendor chunk
-            return 'vendor';
-          }
-        },
+      external: ['react', 'react-dom', 'react-router-dom', 'react-toastify'], // Externalize libraries
+      output: { manualChunks(id) {
+        if (id.includes('node_modules')) {
+          // Group other dependencies into a vendor chunk
+          return 'vendor';
+        }
       },
     },
-    commonjsOptions: {
-      include: /node_modules/, // Ensure compatibility with CommonJS modules
-    },
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Alias for src directory
-    },
+  commonjsOptions: { include: /node_modules/, // Ensure compatibility with CommonJS modules
   },
+},
+resolve: {
+  alias: { '@': path.resolve(__dirname, 'src'), // Alias for src directory
+  },
+},
 });
